494cbead2949f35079e31b11022bbd589b476ced
JTMintsASU <74980613+JTMintsASU@users.noreply.github.com>
2022-02-18 14:00:07 -0700

    LNI Prompt Number/Text Separation
    Removes numbers from LNI Prompts and adds placeholder text for item number.

--- QuizBot/Assets/script/Prompts/Prompts_LNI.cs
+++ QuizBot/Assets/script/Prompts/Prompts_LNI.cs
@@ -9,60 +9,60 @@ public class Prompts_LNI : Array_Prompts
     {
         //See array_prompts for definition
         prompts1 = new string[7] {
-            "(1) Part 2 P",
-            "(2) R",
-            "(3) E",
-            "(4) O",
-            "(5) F",
-            "(6) S",
-            "(7) A"
+            "Part 2 P",
+            "R",
+            "E",
+            "O",
+            "F",
+            "S",
+            "A"
         };
 
         prompts2 = new string[7] {
-            "(1) D",
-            "(2) V",
-            "(3) J",
-            "(4) L",
-            "(5) N",
-            "(6) T",
-            "(7) B"
+            "D",
+            "V",
+            "J",
+            "L",
+            "N",
+            "T",
+            "B"
         };
 
         prompts3 = new string[6] {
-            "(1) H",
-            "(2) Q",
-            "(3) G",
-            "(4) C",
-            "(5) Y",
-            "(6) I"
+            "H",
+            "Q",
+            "G",
+            "C",
+            "Y",
+            "I"
         };
 
         prompts4 = new string[6] {
-            "(1) Astronaut",
-            "(2) Rocket",
-            "(3) Planet",
-            "(4) Fossils",
-            "(5) Tracks",
-            "(6) Scientist"
+            "Astronaut",
+            "Rocket",
+            "Planet",
+            "Fossils",
+            "Tracks",
+            "Scientist"
         };
 
         prompts5 = new string[6] {
-            "(1) U",
-            "(2) X",
-            "(3) W",
-            "(4) M",
-            "(5) K",
-            "(6) Z"
+            "U",
+            "X",
+            "W",
+            "M",
+            "K",
+            "Z"
         };
 
         prompts6 = new string[7] {
-            "(1) N",
-            "(2) F",
-            "(3) H",
-            "(4) W",
-            "(5) P",
-            "(6) I",
-            "(7) Y"
+            "N",
+            "F",
+            "H",
+            "W",
+            "P",
+            "I",
+            "Y"
         };
     }
 }
